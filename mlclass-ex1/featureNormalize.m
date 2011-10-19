6a7eca8e7922216c5cd4cbd37a92b487df0d1fa6
Dan Kalotay <d.kalotay@gmail.com>
2011-10-19 16:44:46 +0200

    completed feature normalize

--- mlclass-ex1/featureNormalize.m
+++ mlclass-ex1/featureNormalize.m
@@ -6,9 +6,6 @@ function [X_norm, mu, sigma] = featureNormalize(X)
 %   working with learning algorithms.
 
 % You need to set these values correctly
-X_norm = X;
-mu = zeros(1, size(X, 2));
-sigma = zeros(1, size(X, 2));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: First, for each feature dimension, compute the mean
@@ -25,9 +22,9 @@ sigma = zeros(1, size(X, 2));
 %
 % Hint: You might find the 'mean' and 'std' functions useful.
 %       
-
-
-
+mu = mean(X);
+sigma = std(X);
+X_norm = bsxfun(@rdivide, bsxfun(@minus, X, mu), sigma);
 
 
 
