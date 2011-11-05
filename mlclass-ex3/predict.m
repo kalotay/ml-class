838ed32974d72b9f269bdb50c982107531f816a0
Dan Kalotay <d.kalotay@gmail.com>
2011-11-05 16:24:25 +0100

    trained neural network prediction

--- mlclass-ex3/predict.m
+++ mlclass-ex3/predict.m
@@ -21,7 +21,8 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
-
+[_, p] = max(sigmoid([ones(m, 1) ,sigmoid(...
+    [ones(m, 1), X] * Theta1')] * Theta2'), [], 2);
 
 
 
