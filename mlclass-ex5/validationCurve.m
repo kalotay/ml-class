760191fafa259106e8e77bfcd12f52d423b4cae0
Dan Kalotay <d.kalotay@gmail.com>
2011-11-20 23:51:51 +0100

    woops, vector indecies

--- mlclass-ex5/validationCurve.m
+++ mlclass-ex5/validationCurve.m
@@ -41,8 +41,8 @@ error_val = zeros(length(lambda_vec), 1);
 for i = 1:length(lambda_vec)
     lambda = lambda_vec(i);
     theta = trainLinearReg(X, y, lambda);
-    error_train = linearRegCostFunction(X, y, theta, 0);
-    error_val = linearRegCostFunction(Xval, yval, theta, 0);
+    error_train(i) = linearRegCostFunction(X, y, theta, 0);
+    error_val(i) = linearRegCostFunction(Xval, yval, theta, 0);
 end
 
 
