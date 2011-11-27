405c2ac3b39b08eb995bc1b983b79d1bbcc87b63
Dan Kalotay <d.kalotay@gmail.com>
2011-11-27 22:51:56 +0100

    gaussian kernel implementation

--- mlclass-ex6/gaussianKernel.m
+++ mlclass-ex6/gaussianKernel.m
@@ -7,7 +7,6 @@ function sim = gaussianKernel(x1, x2, sigma)
 x1 = x1(:); x2 = x2(:);
 
 % You need to return the following variables correctly.
-sim = 0;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the similarity between x1
@@ -15,7 +14,8 @@ sim = 0;
 %               sigma
 %
 %
-
+Difference = x1 - x2;
+sim = exp(- Difference' * Difference / (2 * sigma ^ 2));
 
 
 
