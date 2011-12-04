89454fa56b1a04803bc56a6d5fa6429d6065e955
Dan Kalotay <d.kalotay@gmail.com>
2011-12-04 18:09:18 +0100

    fixed data points plottign to make scatter happy
    see: http://www.ml-class.org/course/qna/view?id=4921

--- mlclass-ex7/plotDataPoints.m
+++ mlclass-ex7/plotDataPoints.m
@@ -5,10 +5,9 @@ function plotDataPoints(X, idx, K)
 %   with the same index assignments in idx have the same color
 
 % Create palette
-palette = hsv(K + 1);
-colors = palette(idx, :);
+colormap(hsv(K + 1)(1:end-1,:));
 
 % Plot the data
-scatter(X(:,1), X(:,2), 15, colors);
+scatter(X(:,1), X(:,2), 15, idx);
 
 end
